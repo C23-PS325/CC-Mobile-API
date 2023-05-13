9b1a15777cba8534353060e20085511cd456b48e
Taufiq Permana <35789663+taufiqpsumarna@users.noreply.github.com>
2023-05-13 12:09:58 +0000

    Database Connection

---
+++ server.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Failed to connect to database:', error);
+    process.exit(1);
+  }
+  console.log('Connected to database.');
+});
+
+// Your application code goes here
