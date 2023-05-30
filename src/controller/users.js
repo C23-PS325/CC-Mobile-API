22fb989f23fdd35a49d25cb42d828d238cdd8a17
almeizaarvin <13519066@std.stei.itb.ac.id>
2023-05-30 16:28:38 +0700

    Add password encrypt

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,4 +1,18 @@
 const connection = require('../utils/db.js')
+const CryptoJS = require('crypto-js');
+
+
+const encryptWithAES = (text) => {
+  const passphrase = '123';
+  return CryptoJS.AES.encrypt(text, passphrase).toString();
+};
+
+const decryptWithAES = (ciphertext) => {
+  const passphrase = '123';
+  const bytes = CryptoJS.AES.decrypt(ciphertext, passphrase);
+  const originalText = bytes.toString(CryptoJS.enc.Utf8);
+  return originalText;
+};
 
 const getAllUsers = (req, res) => {
     const getQuery = 'SELECT * FROM users'
@@ -20,7 +34,7 @@ const getAllUsers = (req, res) => {
 const createNewUser = (req, res) => {
     console.log(req.body)
 
-    const postQuery = `INSERT INTO users (username, email, password, photoUrl) VALUES ('${req.body.username}', '${req.body.email}', '${req.body.password}', '${req.body.photoUrl}'); `
+    const postQuery = `INSERT INTO users (username, email, password, photoUrl) VALUES ('${req.body.username}', '${req.body.email}', '${encryptWithAES(req.body.password)}', '${req.body.photoUrl}'); `
 
     connection.query(postQuery, (err, rows) => {
         if(err){
@@ -38,7 +52,7 @@ const createNewUser = (req, res) => {
 const updateUser = (req, res) => {
     console.log(req.params)
     const patchQuery = `UPDATE users
-    SET username = '${req.body.username}', email = '${req.body.email}', password = '${req.body.password}', photoUrl = '${req.body.photoUrl}'
+    SET username = '${req.body.username}', email = '${req.body.email}', password = '${encryptWithAES(req.body.password)}', photoUrl = '${req.body.photoUrl}'
     WHERE id = '${req.params.id}';
     `
 
