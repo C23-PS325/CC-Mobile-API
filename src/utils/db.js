8f6373030479f148df451de8264880a5a64bbb90
almeizaarvin <13519066@std.stei.itb.ac.id>
2023-05-30 15:02:08 +0700

    Add env

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,19 +2,11 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'communicare',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 
-// Database connection
-connection.connect((error) => {
-  if (error) {
-    console.error('Failed to connect to database:', error);
-    process.exit(1);
-  }
-  console.log('Connected to database.');
-})
 
-module.exports = connection;
+module.exports= connection ;
